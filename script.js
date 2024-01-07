75aff4bec8d7eeffddf193b6877c4459200e29c3
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 02:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function curry(callback) {
-  
-}
+const curry = (callback) => {
+  const curried = (...args) => {
+    if (args.length >= callback.length) {
+      return callback(...args);
+    } else {
+      return (...moreArgs) => curried(...args, ...moreArgs);
+    }
+  };
+
+  return curried;
+};
+
+const sum = (...numbers) => numbers.reduce((total, number) => total + number, 0);
+const curriedSum = curry(sum);
+
+console.log(curriedSum()); // 0
+console.log(curriedSum(1)()); // 1
+console.log(curriedSum(1)(2)()); // 3
+console.log(curriedSum(1, 2)(3)(4, 5, 6)()); // 21
+console.log(curriedSum(1)); // [Function]
+console.log(curriedSum(1, 2)(3)); // [Function]
+
 
 module.export=curry
